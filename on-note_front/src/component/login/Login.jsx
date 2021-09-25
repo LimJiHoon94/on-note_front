4abe9f6f7c1d469f73b20a27fc7e2f823db54f35
임지훈 <boy8820@naver.com>
2021-09-25 14:33:26 +0900

    router add

---
+++ on-note_front/src/component/login/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+//import './Login_style.scss';
+
+
+function Login(){
+
+
+    return(
+        <div>
+            login
+        </div>
+    )
+}
+
+export default Login;
