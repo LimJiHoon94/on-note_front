9155bbfc1cc72e6707c536d582f15adaca2582aa
임지훈 <boy8820@naver.com>
2021-10-30 21:31:54 +0900

    firstContent commit

--- on-note_front/src/component/main/Main.jsx
+++ on-note_front/src/component/main/Main.jsx
@@ -18,6 +18,78 @@ function Main(){
                     </div>
                 </div>
             </div> 
+            
+            <section className="firstSection">
+                <div className="firstComment">
+                    <h1>Cocktail</h1>
+                    <p>
+                        칵테일 레시피를 손쉽게 확인하세요!
+                    </p>
+                </div> 
+                <div className="fitstContent">
+                    <div class="firstContentRow">
+                        <div className="firstContentItem">
+                            <img src="img/campari.jpg"  />
+                            <h6>
+                                캄파리
+                            </h6>
+                            <p className="firstContentItemTag">
+                                #리큐르 #달달 
+                            </p>
+                        </div>
+                        <div className="firstContentItem">
+                            <img src="img/gintonic.jpg"  />
+                            <h6>
+                                진토닉
+                            </h6>
+                            <p className="firstContentItemTag">
+                                #진 #라임 #청량 
+                            </p>
+                        </div>
+                        <div className="firstContentItem">
+                            <img src="img/bodca.jpg"  />
+                            <h6>
+                                스크류 드라이버
+                            </h6>
+                            <p className="firstContentItemTag">
+                                #보드카 #오렌지
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="fitstContent">
+                    <div class="firstContentRow">
+                        <div className="firstContentItem">
+                            <img src="img/sunrise.jpg"  />
+                            <h6>
+                                데킬라 선라이즈
+                            </h6>
+                            <p className="firstContentItemTag">
+                                #데킬라 #오렌지 #그레나딘 
+                            </p>
+                        </div>
+                        <div className="firstContentItem">
+                            <img src="img/manheton.jpg"  />
+                            <h6>
+                                맨하탄
+                            </h6>
+                            <p className="firstContentItemTag">
+                                #버번 #베르뭇 
+                            </p>
+                        </div>
+                        <div className="firstContentItem">
+                            <img src="img/godfather.jpg"  />
+                            <h6>
+                                갓파더
+                            </h6>
+                            <p className="firstContentItemTag">
+                                #아마레또 #위스키 #대부 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+            </section>
         </div> 
     )
 }  
